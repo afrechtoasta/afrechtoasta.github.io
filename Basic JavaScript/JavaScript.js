170d6b777c0c5d3cbd3c7923017a513ff40d4e8a
afrechtoasta <lilalisac@gmail.com>
2026-02-17 20:18:29 -0500

    finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Basic JavaScript/JavaScript.js
+++ Basic JavaScript/JavaScript.js
@@ -7,7 +7,7 @@ document.getElementById("increase").addEventListener("click", function() {
 });
 function tickUp() {
     displayNum.textContent = counter;
-}
+};
 
 document.getElementById("decrease").addEventListener("click", function() {
     counter--;
@@ -15,24 +15,78 @@ document.getElementById("decrease").addEventListener("click", function() {
 });
 function tickDown() {
     displayNum.textContent = counter;
-}
+};
 
-
-document.getElementById("loop").addEventListener("click", function() {
+function runForLoop(){
     let result = "";
     for (let i = 0; i <= counter; i++) {
         result += i + " ";
     }
+
     document.getElementById("forLoopResult").textContent = result;
-});
+};
 
+function showOddNumbercss(){
 
-document.getElementById("odd").addEventListener("click", function() {
-    let result = "";
+   let result = "";
     for (let i = 1; i <= counter; i++) {
         if (i % 2 !== 0) {
             result += i + " ";
         }
     }
     document.getElementById("oddNumberResult").textContent = result;
+
+};
+
+function addMultiplesToArray() {
+
+    let a = [];
+    if (counter >= 5) {
+        for (let i = 5; i <= counter; i += 5) {
+            a.unshift(i); 
+        }
+    }
+    console.log(a); 
+};
+
+function printCarObject() {
+    const type = document.getElementById("carType").value;
+    const mpg = document.getElementById("carMPG").value;
+    const color = document.getElementById("carColor").value;
+
+    const c = {
+        type: type,
+        mpg: mpg,
+        color: color
+    }
+    console.log(c);
+};
+
+function loadCar(d) {
+    const type = document.getElementById("carType").value = d.cType;
+    const mpg = document.getElementById("carMPG").value = d.cMPG;
+    const color = document.getElementById("carColor").value = d.cColor;
+};
+document.getElementById("one").addEventListener("click", function() {
+    loadCar(carObject1);
+});
+document.getElementById("two").addEventListener("click", function() {
+    loadCar(carObject2);
+});
+document.getElementById("three").addEventListener("click", function() {
+    loadCar(carObject3);
+});
+
+
+const story = document.getElementById("styleParagraph");
+document.getElementById("red").addEventListener("click", function() {
+    story.style.color = "red";
+});
+
+document.getElementById("green").addEventListener("click", function() {
+    story.style.color = "green";
+});
+
+document.getElementById("blue").addEventListener("click", function() {
+    story.style.color = "blue";
 });
